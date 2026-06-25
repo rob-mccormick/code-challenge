d3abc1c7624c2405f3b13a26e5479fafcba0df3c
Rob McCormick <51120728+rob-mccormick@users.noreply.github.com>
2026-06-25 11:24:45 +0200

    Update tests to test all sample pages

--- spec/file_scraper_spec.rb
+++ spec/file_scraper_spec.rb
@@ -3,52 +3,63 @@
 require "file_scraper"
 
 RSpec.describe FileScraper do
-  before :all do
-    path = "./files/van-gogh-paintings.html"
-    json_response = FileScraper.run(path)
-    @response = JSON.parse(json_response)
-  end
+  FILE_PATHS = [
+    "./files/van-gogh-paintings.html",
+    "./files/rene-magritte-paintings.html",
+    "./files/gerhard-richter-paintings.html",
+  ]
 
-  let(:expected_response) { JSON.parse(File.read("./files/expected-array.json")) }
+  FILE_PATHS.each do |file_path|
+    context "using #{file_path}" do
+      before :all do
+        json_response = FileScraper.run(file_path)
+        @response = JSON.parse(json_response)
+      end
 
-  it "contains artworks array" do
-    expect(@response["artworks"]).to be_a(Array)
-  end
+      let(:expected_response) { JSON.parse(File.read("./files/expected-array.json")) }
 
-  it "artworks – name" do
-    expect(@response["artworks"].first["name"]).to be_a(String)
-    expect(@response["artworks"].first["name"]).not_to be_empty
-  end
+      it "contains artworks array" do
+        expect(@response["artworks"]).to be_a(Array)
+      end
 
-  it "artworks – extensions" do
-    expect(@response["artworks"].first["extensions"]).to be_a(Array)
-  end
+      it "artworks – name" do
+        expect(@response["artworks"].first["name"]).to be_a(String)
+        expect(@response["artworks"].first["name"]).not_to be_empty
+      end
 
-  it "artworks – link" do
-    expect(@response["artworks"].first["link"]).to be_a(String)
-    expect(@response["artworks"].first["link"]).not_to be_empty
-  end
+      it "artworks – extensions" do
+        expect(@response["artworks"].first["extensions"]).to be_a(Array)
+      end
 
-  context "with thumbnail" do
-    it "artworks – image" do
-      expect(@response["artworks"].first["image"]).to be_a(String)
-      expect(@response["artworks"].first["image"]).not_to be_empty
-    end
-  end
+      it "artworks – link" do
+        expect(@response["artworks"].first["link"]).to be_a(String)
+        expect(@response["artworks"].first["link"]).not_to be_empty
+      end
 
-  context "without thumbnail" do
-    it "artworks – image" do
-      expect(@response["artworks"].last["image"]).to be_a(String)
-      expect(@response["artworks"].last["image"]).not_to be_empty
-    end
-  end
+      context "with thumbnail" do
+        it "artworks – image" do
+          expect(@response["artworks"].first["image"]).to be_a(String)
+          expect(@response["artworks"].first["image"]).not_to be_empty
+        end
+      end
 
-  it "produces the expected response" do
-    @response["artworks"].each.with_index do |artwork, index|
-      expect(artwork["name"]).to eq(expected_response["artworks"][index]["name"])
-      expect(artwork["extensions"]).to eq(expected_response["artworks"][index]["extensions"])
-      expect(artwork["link"]).to eq(expected_response["artworks"][index]["link"])
-      expect(artwork["image"]).to eq(expected_response["artworks"][index]["image"])
+      context "without thumbnail" do
+        it "artworks – image" do
+          expect(@response["artworks"].last["image"]).to be_a(String)
+          expect(@response["artworks"].last["image"]).not_to be_empty
+        end
+      end
+
+      if file_path == "./files/van-gogh-paintings.html"
+        it "produces the expected response" do
+          @response["artworks"].each.with_index do |artwork, index|
+            expect(artwork["name"]).to eq(expected_response["artworks"][index]["name"])
+            expect(artwork["extensions"]).to eq(expected_response["artworks"][index]["extensions"])
+            expect(artwork["link"]).to eq(expected_response["artworks"][index]["link"])
+            expect(artwork["image"]).to eq(expected_response["artworks"][index]["image"])
+          end
+        end
+      end
     end
   end
 end
